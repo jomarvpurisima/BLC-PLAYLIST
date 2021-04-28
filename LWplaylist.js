66cc1cdadb43d4610b4d535c5e48f9441661aed9
marvangux <82187783+jomarvpurisima@users.noreply.github.com>
2021-04-28 09:23:17 +0800

    Update LWplaylist.js

--- LWplaylist.js
+++ LWplaylist.js
@@ -1,39 +1,39 @@
 { "videos" : [
   {
     "title":"Lovely Writer Episode 1",
-    "url":"https://dood.so/e/r75q5cqif9uk19f50su15gos4a07emxc"
+    "url":"//dood.so/e/r75q5cqif9uk19f50su15gos4a07emxc"
   },
   {
     "title":"Lovely Writer Episode 2",
-    "url":"https://dood.so/e/dugnarug75p8r45ty6hwxqzee8nou8sy"
+    "url":"//dood.so/e/dugnarug75p8r45ty6hwxqzee8nou8sy"
   },
   {
     "title":"Lovely Writer Episode 3",
-    "url":"https://dood.so/e/mxkfpnbl0f8au9iqysqjynxaafyt5lm1"
+    "url":"//dood.so/e/mxkfpnbl0f8au9iqysqjynxaafyt5lm1"
   },
   {
     "title":"Lovely Writer Episode 4",
-    "url":"https://dood.so/e/lrlpg012rjucup2hn5zz3fkr9rksux33"
+    "url":"//dood.so/e/lrlpg012rjucup2hn5zz3fkr9rksux33"
   },
   {
     "title":"Lovely Writer Episode 5",
-    "url":"https://dood.so/e/o8m0uivb3fhw7qxdag5b05hihvwdro7j"
+    "url":"//dood.so/e/o8m0uivb3fhw7qxdag5b05hihvwdro7j"
   },
   {
     "title":"Lovely Writer Episode 6",
-    "url":"https://dood.so/e/bm1mk7tlwiw4jeood1smpgsbsqjg87hv"
+    "url":"//dood.so/e/bm1mk7tlwiw4jeood1smpgsbsqjg87hv"
   },
   {
     "title":"Lovely Writer Episode 7",
-    "url":"https://dood.so/e/eynnny6db3056jqz59ilxlwofnjbg00f"
+    "url":"//dood.so/e/eynnny6db3056jqz59ilxlwofnjbg00f"
   },
   {
     "title":"Lovely Writer Episode 8",
-    "url":"https://dood.so/e/m5r0cwev8iyuvgbkx90otayad9ogwk9q"
+    "url":"//dood.so/e/m5r0cwev8iyuvgbkx90otayad9ogwk9q"
   },
   {
     "title":"Lovely Writer Episode 9",
-    "url":"https://dood.so/e/0w2ckowwuepv12wmkd2izcdmx81uepca"
+    "url":"//dood.so/e/0w2ckowwuepv12wmkd2izcdmx81uepca"
   },
  
 ]}
